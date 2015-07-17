0f0dcf7994fb667417b6605aa0764cffcfcd3492
boddicheg <boddicheg@gmail.com>
2015-07-17 16:49:22 +0300

    Comments in doxygen style

--- containers/src/vec.h
+++ containers/src/vec.h
@@ -4,10 +4,10 @@
 namespace containers {
 
 /*!
-     \brief Vector implementation
-     \author boddicheg
-     \version 0.0.1
-     \warning This class is designed for educational purposes only
+     @brief Vector implementation
+     @author boddicheg
+     @version 0.0.1
+     @warning This class is designed for educational purposes only
 
       In some terminology a vector can be described as a dynamic C-style array,
       it offers fast and efficient access to individual elements in any order
@@ -26,40 +26,50 @@ private:
   value_type* m_arr;
   size_t m_size;
   size_t m_capacity;
+  iterator m_begin;
+  iterator m_end;
 
 public:
   /*!
-   * Creates a vector with default constructed elements.
-   * \param[in] size The number of elements to initially create.
-   * \param[in] element An element to copy.
+   * Default constructor creates no elements.
    */
-  vec(size_t size, value_type element) {
-    m_arr = new int[size];
-    for (size_t i = 0; i < size; ++i) {
-      m_arr[i] = element;
-    }
-    m_size = size;
-    m_capacity = size;
-  }
-  // Default constructor creates no elements.
   vec() {
-    m_arr = new int[0];
+    m_arr = new value_type[0];
     m_size = 0;
     m_capacity = 0;
   }
-
+  /*!
+   * Creates a vector with default constructed elements.
+   * @param[in] size The number of elements to initially create.
+   */
   vec(size_t size) {
-    m_arr = new int[size];
+    m_arr = new value_type[size];
     for (size_t i = 0; i < size; ++i) {
       m_arr[i] = 0;
     }
     m_size = size;
     m_capacity = size;
   }
-
+  /*!
+   * Creates a vector with default constructed elements.
+   * @param[in] size The number of elements to initially create.
+   * @param[in] element An element to copy.
+   */
+  vec(size_t size, const value_type& element) {
+    m_arr = new value_type[size];
+    for (size_t i = 0; i < size; ++i) {
+      m_arr[i] = element;
+    }
+    m_size = size;
+    m_capacity = size;
+  }
+  /*!
+   * Vector copy constructor.
+   * @param[in] other A vector of identical elemen
+   */
   vec(const vec<value_type>& other) {
     size_t size = (other.size() < other.capacity()) ? other.capacity() : other.size();
-    m_arr = new int[size];
+    m_arr = new value_type[size];
     m_size = other.size();
     m_capacity = other.capacity();
     for (size_t i = 0; i < other.size(); ++i) {
@@ -69,22 +79,53 @@ public:
     // @TODO: init iterators
   }
 
+  /*!
+   * The dtor only erases the elements, and note that if the elements themselves
+   * are pointers, the pointed-to memory is not touched in any way.
+   * Managing the pointer is the user's responsibility.
+   */
   ~vec() {
+    // @TODO: fix
     delete[] m_arr;
   }
-
+  /*!
+   * Returns size of vector
+   */
   size_t size() const { return m_size; }
+  /*!
+   * Returns current capacity
+   */
   size_t capacity() const { return m_capacity; }
+  /*!
+   * Returns true if size of vector > 0, overwise false
+   */
+  bool empty() const { return m_size == 0; }
 
-  value_type& operator[](int index) noexcept {
+  /*!
+   * Subscript access to the data contained in the vector.
+   * No index check
+   * @param  index Index in vector
+   * @return       Element
+   */
+  reference operator[](int index) noexcept {
     return m_arr[index];
   }
-
-  value_type& operator[](int index) const noexcept {
+  /*!
+   * Read access to the data contained in vector
+   * No index check
+   * @param  index Index in vector
+   * @return       Element
+   */
+  const_reference operator[](int index) const noexcept {
     return m_arr[index];
   }
-
-  value_type& at(int index) {
+  /*!
+   * Full access to element in vector
+   * Generate out_of_range id index >= size
+   * @param  index Index in vector
+   * @return       Element
+   */
+  reference at(int index) {
     if (index >= m_size) {
       // @TODO: fix memory leak
       throw std::out_of_range("vec::_M_range_check");
