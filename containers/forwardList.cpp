5e2e46f4c4dc23af4d1c3809f77f01a04f87e506
Gavin Goodship <msgoodship1@gmail.com>
2016-01-27 23:45:53 +0000

    Update forwardList.cpp

--- containers/forwardList.cpp
+++ containers/forwardList.cpp
@@ -1,12 +1,11 @@
 #include "./utils.h"
 
 
-/* forward_list */
 void challenge(){
   // o(1)
   forward_list<int> listOne;
-  
-  print<forward_list<int>>(listOne);
-  // o(n)
-  
-}
+  print(forward_list<int>(listOne));//prints empty list 
+  //o(n) linear time
+  forward_list<int> listTwo(1,2);//takes one element of two
+  print(forward_list<int>(listTwo));//prints 2 
+  // o(n) linear time
