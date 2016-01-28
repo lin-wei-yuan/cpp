74be2cae967565a36c5d6185f1904f9e9999630a
Gavin Goodship <msgoodship1@gmail.com>
2016-01-28 22:44:26 +0000

    Update Deque.cpp

--- containers/Deque.cpp
+++ containers/Deque.cpp
@@ -1,1 +1,13 @@
 
+#include "./utils.h"
+
+void challenge(){
+  deque<int> dq1;
+  dq1.push_front(1);//push 1 to front of the deque at constant time
+  dq1.push_back(10);//push 10 to the back of the deque at constant time 
+  print(deque<int>(dq1));//prints the elements of 1,10
+  deque<int> dq2;//creates dq2
+  dq2.resize(10);//resize to take 10 elements 
+  print(deque<int>(dq2));//prints 10 zeros
+  
+}
