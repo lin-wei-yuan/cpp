53ac6efb74dd8bbfc36824e5d6dabbe4be8cb0b5
Gavin Goodship <msgoodship1@gmail.com>
2016-01-29 01:11:12 +0000

    Update LexicalCast.cpp

--- Boost/LexicalCast.cpp
+++ Boost/LexicalCast.cpp
@@ -1,2 +1,10 @@
 #include <boost/lexical_cast.hpp>
 #include "./utils.h"
+#include<string>
+using boost::lexical_cast;
+
+void challenge(){
+   int numberOne =260;//int definition
+   string strOne =lexical_cast<string>(numberOne);//converts numberOne to str
+   cout<<strOne;//prints the StrOne
+}
