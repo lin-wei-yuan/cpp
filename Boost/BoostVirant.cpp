2fad09ad39a93311e74a2cfc7781a48cf6e2ac5a
Gavin Goodship <msgoodship1@gmail.com>
2016-01-31 19:53:13 +0000

    Update BoostVirant.cpp

--- Boost/BoostVirant.cpp
+++ Boost/BoostVirant.cpp
@@ -1,2 +1,14 @@
+#include<string>
+#include<iostream>
 #include "boost/variant.hpp"
-using 
+using boost::variant
+void challenge(){
+  variant<string,double,int> Var;
+  Var = "hello";
+  cout<<Var<<endl;
+  Var = 3.13;//
+  cout<<Var<<endl;//prints var
+  Var =21;//makes var =21
+  cout<<Var<<endl;//prints 21
+  cout<<boost::get<int>(Var)<<endl;//prints the variant value of the int hence 21
+}
