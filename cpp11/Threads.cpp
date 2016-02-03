895c841f010c780a64e18997257b09d9b45adfda
Gavin Goodship <msgoodship1@gmail.com>
2016-02-03 21:46:19 +0000

    Update Threads.cpp

--- cpp11/Threads.cpp
+++ cpp11/Threads.cpp
@@ -4,3 +4,8 @@ using namespace std;
 void challege(){
   cout<<"this is challege function"<<endl;
 }
+
+int main(){
+  thread threadOne(Challenge);
+  threadOne.join();//waits for the child thread to finsh
+}
