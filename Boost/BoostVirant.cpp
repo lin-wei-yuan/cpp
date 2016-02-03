08bf66840f4abc5b2a2aed8500cf03ee8adc8561
Gavin Goodship <msgoodship1@gmail.com>
2016-02-03 21:07:55 +0000

    Update BoostVirant.cpp

--- Boost/BoostVirant.cpp
+++ Boost/BoostVirant.cpp
@@ -1,23 +1,26 @@
+
 #include<string>
 #include<iostream>
 #include "boost/variant.hpp"
-using boost::variant
+
+using boost::variant;
 void challenge(){
-  variant<string,double,int> Var;
+  variant<std::string,double,int> Var;
   Var = "hello";
-  cout<<Var<<endl;
-  Var = 3.13;//
-  cout<<Var<<endl;//prints var
+ std::cout<<Var<<std::endl;
+  Var = 3.13;
+  std::cout<<Var<<std::endl;//prints var
   Var =21;//makes var =21
-  cout<<Var<<endl;//prints 21
-  cout<<boost::get<int>(Var)<<endl;//prints the variant value of the int hence 21
+  std::cout<<Var<<std::endl;//prints 21
+  std::cout<<boost::get<int>(Var)<<std::endl;//prints the variant value of the int hence 21
 }
 
 struct ShowVariant: public boost::static_visitor<>{
-  ShowVariant()(int I){ cout<<I<<endl;}
-  ShowVariant()(string g){cout<<g<<endl;}
+  void operator()(int I){ std::cout<<I<<std::endl;}
+  void operator()(std::string g){std::cout<<g<<std::endl;}
 };
-
 int main(){
-  
-}
+   boost::variant<int,std::string> var;
+   var = "hello world";
+   boost::apply_vistor<std::string>(showVariant{},var)
+   }
