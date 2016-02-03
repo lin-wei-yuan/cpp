9b7460f37a3bd1a651507492611a6925345ace81
Gavin Goodship <msgoodship1@gmail.com>
2016-02-03 22:23:27 +0000

    Update Threads.cpp

--- cpp11/Threads.cpp
+++ cpp11/Threads.cpp
@@ -5,7 +5,21 @@ void challege(){
   cout<<"this is challege function"<<endl;
 }
 
+void challengeTwo(int number,string name){
+   mutex myMutex;//creates my mutex
+   myMutex.lock();//locks the cout from other threads
+   for(int i=0; i<number; ++i){
+    cout<<name<<endl;//prints name 100 times
+    }//closing of for loop
+    myMutex.unlock();//unlocks mutex once thread is done
+}//closing of the challenge function
+
+
 int main(){
-  thread threadOne(Challenge);
-  threadOne.join();//waits for the child thread to finsh
+   thread threadOne(Challenge);
+   thread myThread(challengeTwo,10,"gavin");//thread object taking three parametres
+   thread myThread2(challengeTwo,10,"goodship");//thread object taking three parametres
+   myThread.join();//main waits for mythread to finsh
+   myThread2.join();//main waits for mythread2 to finish
+   threadOne.join();//waits for the child thread to finsh
 }
