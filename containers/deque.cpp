f324533ec09a64203c8b1b200f5b1b22cdf37323
boddicheg <boddicheg@gmail.com>
2016-02-04 23:04:29 +0200

    Deque basic methods

--- containers/deque.cpp
+++ containers/deque.cpp
@@ -19,35 +19,69 @@ void challenge0() {
   // O(n)
   dq5.assign(5, 6);
   print<deque<int>>(dq5);
-  // O(n)
+  // O(n) - between first and last
   dq5.assign(dq1.begin(), dq1.end() - 1);
   print<deque<int>>(dq5);
+  // O(n)
+  deque<int> dq6 = dq1;
+  print<deque<int>>(dq6);
+  // O(1)!
+  deque<int> dq7 = move(dq1);
+  print<deque<int>>(dq7);
+  // Empty after moving
+  print<deque<int>>(dq1);
 }
 
-void challenge(){
+void challenge1() {
   deque<int> dq1;
-  dq1.push_front(1); // O(1)
-  dq1.push_back(10); // O(1)
+  for (int i = 0; i < 10; ++i) {
+    // O(1)
+    dq1.push_front(i);
+    // O(1)
+    dq1.push_back(i);
+  }
   print<deque<int>>(dq1);
+}
 
+void challenge2(){
+  deque<int> dq1;
+  // O(1)
+  dq1.push_front(1);
+  // O(1)
+  dq1.push_back(10);
+  print<deque<int>>(dq1);
   deque<int> dq2;
-  dq2.resize(10);//resize to take 10 elements
-  print(deque<int>(dq2));//prints 10 zeros
+  // O(n)
+  dq2.resize(10);
+  print(deque<int>(dq2));
+
   deque<int> dq3;
   for(int i=0; i<10; ++i){
-      dq3.push_front(i);
-      dq3.push_back(i);
-    }
-    print(deque<int>(dq3));
-    dq3.pop_back();//pops the back
-    dq3.pop_front();//pops the front;
-    print(deque<int>(dq3));//prints dq3
-    auto it = dq3[0]; //it points to value 8
-    cout<<it;//prints the to value 8
-
+    // O(1)
+    dq3.push_front(i);
+    // O(1)
+    dq3.push_back(i);
+  }
+  print(deque<int>(dq3));
+  // O(1)
+  dq3.pop_back();
+  // O(1)
+  dq3.pop_front();
+  print(deque<int>(dq3));
+  // O(1)!
+  auto it = dq3[0];
+  try {
+    // O(1) with cheking range
+    auto it = dq3.at(dq3.size() + 1);
+    cout << it << endl;
+  } catch (const exception& e) {
+    cout << e.what() << endl;
+  }
 }
 
 int main () {
   challenge0();
+  challenge1();
+  challenge2();
   return 0;
 }
