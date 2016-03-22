9e5cbbe53a1af18917a2aca2bc9b1af9bad2e6a6
boddicheg <boddicheg@gmail.com>
2016-03-22 05:09:00 -0400

    Casts

--- oop/cast.cpp
+++ oop/cast.cpp
@@ -1,38 +1,80 @@
+#include <iostream>
+
+#define IS_VIRTUAL
+
 class B {
-  virtual void f() {}
+public:
+  B() { std::cout << "B" << std::endl; }
+  ~B() { std::cout << "~B" << std::endl; }
+  #ifdef IS_VIRTUAL
+    virtual void tmp() const { std::cout << "B::tmp" << std::endl; }
+  #else
+    void tmp() const { std::cout << "B::tmp" << std::endl; }
+  #endif
 };
 
-class V {
-  virtual void g() {}
+class D : public B {
+public:
+  D() { std::cout << "D" << std::endl; }
+  ~D() { std::cout << "~D" << std::endl; }
+  #ifdef IS_VIRTUAL
+    virtual void tmp() const override { std::cout << "D::tmp" << std::endl; }
+  #else
+    void tmp() const { std::cout << "D::tmp" << std::endl; }
+  #endif
+  // Additional method
+  void additional() const { std::cout << "D::additional" << std::endl; }
 };
 
-class X {
+class D1 : public D {
+  D1() { std::cout << "D1" << std::endl; }
+  ~D1() { std::cout << "~D1" << std::endl; }
+  #ifdef IS_VIRTUAL
+    virtual void tmp() const override { std::cout << "D1::tmp" << std::endl; }
+  #else
+    void tmp() const { std::cout << "D1::tmp" << std::endl; }
+  #endif
 };
 
-class D : public B, virtual public V, virtual public X {
-};
-
-int challenge1() {
+void challenge1() {
   D d;
-
+  // get pointer of B type
   B* pb = &d;
-  D* p1 = (D*)pb;
-  D* p2 = dynamic_cast<D*>(pb);
-
-  V* pv = &d;
-  D* p3 = (D*)pv;
-  D* p4 = dynamic_cast<D*>(pv);
-
-  X* px = &d;
-  D* p5 = (D*)px;
-  D* p6 = dynamic_cast<D*>(px);
-
-  return 0;
+  // If tmp virtual -> D::tmp
+  // else B::tmp
+  pb->tmp();
 }
 
+void challenge2() {
+  D d;
+  // C-style cast
+  // Static_cast
+  // reinpreter_cast
+  // const_cast
+  D* p1 = (D*)&d;
+  B* p2 = (B*)&d;
+  B* p3 = (D*)&d;
+  // D* p4 = (B*)&d; error: invalid conversion from
+
+  // If tmp virtual -> D::tmp
+  // else D::tmp
+  p1->tmp();
+  // If tmp virtual -> D::tmp
+  // else B::tmp
+  p2->tmp();
+  // If tmp virtual -> D::tmp
+  // else B::tmp
+  p3->tmp();
+}
+
+void challenge3() {
+  D d;
+  // @todo: dynamic_cast
+}
 
 int main()
 {
-  challenge1();
+  // challenge1();
+  challenge2();
   return 0;
 }
