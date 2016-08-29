82646d5ce70c829529b3200d17f099247e3dd604
boddicheg <boddicheg@gmail.com>
2016-08-29 18:02:25 +0300

    Learning concurrency(work in progress)

---
+++ concurrency/basics.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <thread>
+#include "../basic/basic_utils.h"
+
+using basic_utils::typeof;
+using basic_utils::Test;
+
+void challenge1()
+{
+    // Ctor were called 3 times
+    // Default ctor( default with params if we call with int param)
+    Test worker; 
+    // Copy ctor and move ctor( why? )
+    // we pass lvalue to thread ctor and thats why worker obj is copied 
+    std::thread t1{ worker };
+    if( t1.joinable() )
+    {
+        t1.join();
+    }
+    // Now we have 2 forwards
+    std::thread t2{ std::move(worker) };
+    if( t2.joinable() )
+    {
+        t2.join();
+    }
+
+    // Use lambda 
+    std::thread t3{ []() { std::cout << "I am lambda!" << std::endl; } };
+    if( t3.joinable() )
+    {
+        t3.join();
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    challenge1();
+    return 0;
+}
